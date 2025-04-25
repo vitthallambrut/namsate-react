1dde3d165ab46307c8db48d44fd045f2534ebf01
vitthal.bb4158 <vitthal.lambrut@firstcry.in>
2025-04-25 13:22:03 +0530

    done episode-3

--- App.js
+++ App.js
@@ -18,12 +18,12 @@ import { ReactDOM } from 'react-dom'
 
 
 
-const parent = React.createElement("div",{id:"parent"},
-    React.createElement("div",{id:"child"},['Hello, world!','Hello, world!']),
-    React.createElement("div",{id:"child2"},['Hello, world!','Hello, world!'])
-)
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(parent);
+// const parent = React.createElement("div",{id:"parent"},
+//     React.createElement("div",{id:"child"},['Hello, world!','Hello, world!']),
+//     React.createElement("div",{id:"child2"},['Hello, world!','Hello, world!'])
+// )
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(parent);
 
 
 
@@ -31,3 +31,22 @@ root.render(parent);
 // const heading = React.createElement('h1', {id:"heading"}, 'Hello, world!');
 // const root = ReactDOM.createRoot(document.getElementById('root'));
 // root.render(heading);
+const num = 10;
+const Title = () => {
+    return (
+        <h1>Hello, world! From Title</h1>
+    )
+}
+
+const Heading = () => {
+    return (
+        <div>
+            {num}
+            <Title />
+            <h1>Hello, world!</h1>
+        </div>
+    )
+
+}
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Heading />);
