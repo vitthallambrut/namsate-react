9a32879ae06406036fcf99a9de43b18e5b05cc93
vitthal.bb4158 <vitthal.lambrut@firstcry.in>
2025-04-26 14:50:33 +0530

    added css

--- App.js
+++ App.js
@@ -31,22 +31,64 @@ import { ReactDOM } from 'react-dom'
 // const heading = React.createElement('h1', {id:"heading"}, 'Hello, world!');
 // const root = ReactDOM.createRoot(document.getElementById('root'));
 // root.render(heading);
-const num = 10;
-const Title = () => {
-    return (
-        <h1>Hello, world! From Title</h1>
-    )
-}
 
-const Heading = () => {
+const Header = () => {
     return (
-        <div>
-            {num}
-            <Title />
-            <h1>Hello, world!</h1>
+        <div className='header'>
+            <div>
+                <img className='logo' src="https://cdn2.f-cdn.com/contestentries/2426851/74260034/6689bc27dfb41_thumb900.jpg" alt="logo" />
+            </div>
+            <div className='nav-items'>
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
         </div>
     )
-
 }
+const ResturantCard = () => {
+    return (
+        <div className='res-card'>
+            <img className='img' src="https://www.shutterstock.com/image-photo/fried-salmon-steak-cooked-green-600nw-2489026949.jpg" width={250} height={150} alt="Resturant" />
+            <h3>Meghana Foods</h3>
+            <h4>Burger, North Indian, Asian</h4>
+            <h4>4.4 stars</h4>
+            <h4>38 minutes</h4>
+        </div>
+    )
+}
+const Body = () => {
+    return (
+        <div className='body'>
+            <div className='search'>Search</div>
+            <div className='res-container'>
+                <ResturantCard />
+                <ResturantCard />
+                <ResturantCard />
+                <ResturantCard />
+                <ResturantCard />
+                <ResturantCard />
+                <ResturantCard />
+                <ResturantCard />
+                <ResturantCard />
+            </div>
+        </div>
+
+    )
+}
+
+const AppLayout = () => {
+    return (
+        <div className='app'>
+            <Header />
+            <Body />
+        </div>
+    )
+}
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Heading />);
+root.render(<AppLayout />);
